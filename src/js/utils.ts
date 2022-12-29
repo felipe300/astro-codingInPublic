c5439d5b7c65d5446c82bd8459b427c402958188
Felipe300 <caracoloro@gmail.com>
2022-12-29 13:19:12 -0300

    feat: add filter for posts

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,3 +1,6 @@
+import type { MarkdownInstance } from "astro"
+import type { Frontmatter } from "src/types/types"
+
 export function sluglify (text: string) {
   return text
     .toString()
@@ -14,4 +17,33 @@ export function formatDate (date: string) {
   return new Date(date).toLocaleDateString('es-cl', {
     timeZone: "UTC"
   })
+}
+
+export function formatBlogPosts (posts: MarkdownInstance<Frontmatter>[], {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined as unknown as number
+} = {}) {
+  const filteredPosts = posts.reduce((acc: MarkdownInstance<Frontmatter>[], post) => {
+    const { date, draft } = post.frontmatter
+    if (filterOutDrafts && draft) return acc
+
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc
+
+    acc.push(post)
+    return acc
+  }, [])
+
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.frontmatter.date).valueOf() - new Date(a.frontmatter.date).valueOf())
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  if (typeof limit === 'number') {
+    return filteredPosts.slice(0, limit)
+  }
+
+  return filteredPosts
 }
