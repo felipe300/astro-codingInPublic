bc2b6072982bd77b4825d3157f849521fe65e32c
Felipe300 <caracoloro@gmail.com>
2023-01-27 16:32:02 -0300

    fix: add astr/image integration v.0.13.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap"
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-awesome-blog.netlify.app/",
   integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
     sitemap(),
   ],
 })
